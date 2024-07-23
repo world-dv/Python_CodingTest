627eb6b7a773178ac1415c086dda7be9f068b258
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-07-23 22:12:04 +0900

    Create 6603.py

---
+++ 6603.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+while True:
+    arr = list(map(int, input().split()))
+    if arr[0] == 0:
+        break
+    for i in combinations(arr[1:], 6):
+        print(*i)
+    print()
